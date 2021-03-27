14f4b033282e9b32af76af3edf12476d3838b092
Antony Helson Chandy <antonyhelsonchandy@Antonys-MacBook-Air.local>
2021-03-27 05:12:59 +0000

    added new class

---
+++ src/main/java/com/example/tranformer/GridRequestTransformer.java
@@ -0,0 +1,9 @@
+package com.example.tranformer;
+
+import org.apache.logging.log4j.LogManager;
+
+import java.util.logging.Logger;
+
+public class GridRequestTransformer {
+private static final Logger LOGGER= (Logger) LogManager.getLogger(GridRequestTransformer.class);
+}
