cae2be8b8cfa780ef47a6899815657242a6f613b
Antony Helson Chandy <antonyhelsonchandy@Antonys-MacBook-Air.local>
2021-03-27 05:25:50 +0000

    new stuff

--- src/main/java/com/example/tranformer/GridRequestTransformer.java
+++ src/main/java/com/example/tranformer/GridRequestTransformer.java
@@ -1,9 +1,14 @@
 package com.example.tranformer;
 
+import com.example.demo.GenericProfilAxonRequest;
+import com.example.demo.GridProfileRequestsMap;
 import org.apache.logging.log4j.LogManager;
 
 import java.util.logging.Logger;
 
 public class GridRequestTransformer {
 private static final Logger LOGGER= (Logger) LogManager.getLogger(GridRequestTransformer.class);
+public GridProfileRequestsMap tranform(final GenericProfilAxonRequest genericProfileAxonRequest){
+return new GridProfileRequestsMap();
+}
 }
